3f2de46f09cdd674ffc63dca169614dd5c53265f
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-11 20:46:42 -0300

    ae2

---
+++ AE2/InputPrintIntStr.py
@@ -0,0 +1,14 @@
+numero = 5
+numero = numero + 5
+print("el resultado es:", numero)
+
+print("Ingrese su nombre:")
+nombre = input()
+
+apellido = input("Ingrese su apellido:") # Devuelve un String o str
+print("Su nombre:", nombre,apellido)
+
+edad = input("ingrese la edad")
+edad = int(edad) + 2
+print("la edad de", nombre,apellido,"es", edad)
+print("edad es" + str(edad))
